8f8025533f4afa8d980aaf6b5c61a79c203395ab
SeonghunYang <didtjdgns852@naver.com>
2024-07-27 07:03:22 +0000

    feat: Add Button component

---
+++ src/headless/components/button/button.tsx
@@ -0,0 +1,17 @@
+import { AriaButtonOptions, useButton } from "react-aria";
+
+interface ButtonProps extends AriaButtonOptions<"button"> {
+  buttonRef: React.RefObject<HTMLButtonElement>;
+  style?: React.CSSProperties | undefined;
+  children: React.ReactNode;
+}
+
+export function Button(props: ButtonProps) {
+  let ref = props.buttonRef;
+  let { buttonProps } = useButton(props, ref);
+  return (
+    <button {...buttonProps} ref={ref} style={props.style}>
+      {props.children}
+    </button>
+  );
+}
