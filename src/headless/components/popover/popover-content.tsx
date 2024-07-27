0a61a3cc72d1e0469360c5c1a208b7f2de8b689d
SeonghunYang <didtjdgns852@naver.com>
2024-07-27 07:45:00 +0000

    refactor: Update PopoverContent component to use react-aria and popover context

--- src/headless/components/popover/popover-content.tsx
+++ src/headless/components/popover/popover-content.tsx
@@ -16,21 +16,25 @@ export function PopoverContent({ children }: PopoverContentProps) {
   } = usePopoverContext();
 
   return (
-    <Overlay>
-      <div {...underlayProps} className="underlay" />
-      <div {...popoverProps} ref={popoverRef} className="popover">
-        <svg
-          {...arrowProps}
-          className="arrow"
-          data-placement={placement}
-          viewBox="0 0 12 12"
-        >
-          <path d="M0 0 L6 6 L12 0" />
-        </svg>
-        <DismissButton onDismiss={state.close} />
-        {children}
-        <DismissButton onDismiss={state.close} />
-      </div>
-    </Overlay>
+    <>
+      {state.isOpen && (
+        <Overlay>
+          <div {...underlayProps} className="underlay" />
+          <div {...popoverProps} ref={popoverRef} className="popover">
+            <svg
+              {...arrowProps}
+              className="arrow"
+              data-placement={placement}
+              viewBox="0 0 12 12"
+            >
+              <path d="M0 0 L6 6 L12 0" />
+            </svg>
+            <DismissButton onDismiss={state.close} />
+            {children}
+            <DismissButton onDismiss={state.close} />
+          </div>
+        </Overlay>
+      )}
+    </>
   );
 }
