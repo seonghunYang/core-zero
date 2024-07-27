8934f5d5da1ea3375d5deea338879835b3261ded
SeonghunYang <didtjdgns852@naver.com>
2024-07-27 05:45:42 +0000

    chore: Refactor useControlledState hook to handle undefined values consistently

--- src/headless/hooks/use-controlled-state.hook.ts
+++ src/headless/hooks/use-controlled-state.hook.ts
@@ -19,16 +19,34 @@ export function useControlledState<T = any>({
 
   const currentValue = isControlled ? value : state;
 
-  const setValue: React.Dispatch<SetStateAction<T>> = useCallback(
-    (newValue, ...args) => {
-      if (!isControlled) {
-        setState(newValue);
-      } else {
+  const setValue: (value: SetStateAction<T>) => void = useCallback(
+    (value, ...args) => {
+      const onChangeCaller = (value: T, ...args: any[]) => {
         if (onChange) {
           if (!Object.is(currentValue, value)) {
             onChange(value, ...args);
           }
         }
+      };
+
+      if (typeof value === "function") {
+        const updateFunction = (oldValue: T) => {
+          const interceptedValue = (value as (prevState: T) => T)(
+            isControlled ? currentValue : oldValue
+          );
+          onChangeCaller(interceptedValue, ...args);
+          if (!isControlled) {
+            return interceptedValue;
+          }
+          return oldValue;
+        };
+
+        setState(updateFunction);
+      } else {
+        if (!isControlled) {
+          setState(value);
+        }
+        onChangeCaller(value, ...args);
       }
     },
     []
