894f1501f86a6c1f284cc79659075ef14e70ff90
SeonghunYang <didtjdgns852@naver.com>
2024-07-27 05:56:08 +0000

    Refactor useControlledState hook to handle undefined values consistently

--- src/__test__/headless/hooks/use-controlled-state.hook.test.ts
+++ src/__test__/headless/hooks/use-controlled-state.hook.test.ts
@@ -3,12 +3,15 @@ import { renderHook } from "@testing-library/react";
 import { useControlledState } from "../../../headless/hooks/use-controlled-state.hook";
 import { act, useState } from "react";
 
-function controlledHook() {
+function controlledHook(props?: { onChange?: (v: boolean) => void }) {
   const [_value, _setValue] = useState(false);
   const [value, setValue] = useControlledState({
     value: _value,
     defaultValue: _value,
-    onChange: (v) => _setValue(v),
+    onChange: (v) => {
+      props?.onChange && props.onChange(v);
+      _setValue(v);
+    },
   });
   return {
     value,
@@ -18,105 +21,102 @@ function controlledHook() {
 }
 
 describe("useControlledState", () => {
-  it("unControl", () => {
-    const { result } = renderHook(() => {
-      const [value, setValue] = useControlledState({ defaultValue: false });
-      return {
-        value,
-        setValue,
-      };
-    });
-
-    act(() => {
-      result.current.setValue(true);
-    });
-
-    expect(result.current.value).toBe(true);
-  });
-
-  it("control-internal hook", () => {
-    const { result } = renderHook(() => ({
-      ...controlledHook(),
-    }));
-
-    act(() => {
-      result.current.setValue(true);
-    });
-
-    expect(result.current.value).toBe(true);
-  });
-
-  it("control-external hook", () => {
-    const { result } = renderHook(() => ({
-      ...controlledHook(),
-    }));
-
-    act(() => {
-      result.current._setValue(true);
-    });
-
-    expect(result.current.value).toBe(true);
-  });
-
-  it("control-function callback", () => {
-    const { result } = renderHook(() => ({
-      ...controlledHook(),
-    }));
-
-    act(() => {
-      result.current.setValue((v) => !v);
-    });
-
-    expect(result.current.value).toBe(true);
-  });
-
-  it("unControl-function callback-onChangeCallback", () => {
-    const { result } = renderHook(() => {
-      const [callbackCall, setCallbackCall] = useState(false);
-      const [value, setValue] = useControlledState({
-        defaultValue: false,
-        onChange: (v) => setCallbackCall(v),
+  describe("unControlled", () => {
+    it("unControl", () => {
+      const { result } = renderHook(() => {
+        const [value, setValue] = useControlledState({ defaultValue: false });
+        return {
+          value,
+          setValue,
+        };
       });
-      return {
-        callbackCall,
-        value,
-        setValue,
-      };
-    });
 
-    act(() => {
-      result.current.setValue((v) => !v);
-    });
+      act(() => {
+        result.current.setValue(true);
+      });
 
-    expect(result.current.value).toBe(true);
-    expect(result.current.callbackCall).toBe(true);
+      expect(result.current.value).toBe(true);
+    });
+    it("control-function callback-onChangeCallback", () => {
+      const { result } = renderHook(() => {
+        const [callbackCall, setCallbackCall] = useState(false);
+
+        return {
+          callbackCall,
+          ...controlledHook({
+            onChange: (v) => {
+              setCallbackCall(v);
+            },
+          }),
+        };
+      });
+
+      act(() => {
+        result.current.setValue((v) => !v);
+      });
+
+      expect(result.current.value).toBe(true);
+      expect(result.current.callbackCall).toBe(true);
+    });
   });
 
-  it("control-function callback-onChangeCallback", () => {
-    const { result } = renderHook(() => {
-      const [callbackCall, setCallbackCall] = useState(false);
-      const [_value, _setValue] = useState(false);
-      const [value, setValue] = useControlledState({
-        value: _value,
-        defaultValue: _value,
-        onChange: (v) => {
-          _setValue(v);
-          setCallbackCall(v);
-        },
+  describe("control", () => {
+    it("control-internal hook", () => {
+      const { result } = renderHook(() => ({
+        ...controlledHook(),
+      }));
+
+      act(() => {
+        result.current.setValue(true);
       });
-      return {
-        value,
-        callbackCall,
-        setValue,
-        _setValue,
-      };
+
+      expect(result.current.value).toBe(true);
     });
 
-    act(() => {
-      result.current.setValue((v) => !v);
+    it("control-external hook", () => {
+      const { result } = renderHook(() => ({
+        ...controlledHook(),
+      }));
+
+      act(() => {
+        result.current._setValue(true);
+      });
+
+      expect(result.current.value).toBe(true);
     });
 
-    expect(result.current.value).toBe(true);
-    expect(result.current.callbackCall).toBe(true);
+    it("control-function callback", () => {
+      const { result } = renderHook(() => ({
+        ...controlledHook(),
+      }));
+
+      act(() => {
+        result.current.setValue((v) => !v);
+      });
+
+      expect(result.current.value).toBe(true);
+    });
+
+    it("unControl-function callback-onChangeCallback", () => {
+      const { result } = renderHook(() => {
+        const [callbackCall, setCallbackCall] = useState(false);
+        const [value, setValue] = useControlledState({
+          defaultValue: false,
+          onChange: (v) => setCallbackCall(v),
+        });
+        return {
+          callbackCall,
+          value,
+          setValue,
+        };
+      });
+
+      act(() => {
+        result.current.setValue((v) => !v);
+      });
+
+      expect(result.current.value).toBe(true);
+      expect(result.current.callbackCall).toBe(true);
+    });
   });
 });
