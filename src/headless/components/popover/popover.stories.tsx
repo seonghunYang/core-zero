f7f7effe8ed7ba34282cd8b09b06b22e37910fe2
SeonghunYang <didtjdgns852@naver.com>
2024-07-29 09:29:56 +0000

    refactor: Update Popover component to use the new PopoverRoot component and context

--- src/headless/components/popover/popover.stories.tsx
+++ src/headless/components/popover/popover.stories.tsx
@@ -1,8 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Popover } from "./popover";
-import React from "react";
-import { useOverlayTriggerState } from "../../hooks/use-overlay-trigger-state.hook";
+import { Popover } from ".";
 
 const meta = {
   title: "Headless/Components/Popover",
@@ -16,13 +14,13 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const render = () => {
-  const state = useOverlayTriggerState({ defaultOpen: false });
-  const triggerRef = React.useRef<HTMLButtonElement>(null);
   return (
     <>
-      <button ref={triggerRef}>click</button>
-      <Popover triggerRef={triggerRef} state={state}>
-        <div>Popover content</div>
+      <Popover>
+        <Popover.Trigger>click click</Popover.Trigger>
+        <Popover.Content>
+          <div>Popover content</div>
+        </Popover.Content>
       </Popover>
     </>
   );
