14bf60105ce9f7167fbff485d2d6fa1fa97aabe3
SeonghunYang <didtjdgns852@naver.com>
2024-07-29 10:02:10 +0000

    refactor: Update usePopover hook to use react-aria for overlay and popover functionality

--- src/headless/hooks/use-popover.hook.ts
+++ src/headless/hooks/use-popover.hook.ts
@@ -1,11 +1,46 @@
+import { useRef } from "react";
 import { useOverlayState } from "./use-overlay-state.hook";
+import type { OverlayState } from "./use-overlay-state.hook";
+import {
+  useOverlayTrigger as useOverlayTriggerAria,
+  usePopover as usePopoverAria,
+} from "react-aria";
 
-// interface Use
+// 타입이 컴포넌트와 결국 같아야함(혹은 가져와야함)
+interface UsePopoverArgs {
+  state?: OverlayState;
+}
 
-// export function usePopover() {
-//   const state = useOverlayState({
-//     isOpen: props.state?.isOpen,
-//     defaultOpen: false,
-//     onOpenChange: props.state?.setOpen,
-//   });
-// }
+export function usePopover(props: UsePopoverArgs) {
+  const state = useOverlayState({
+    isOpen: props.state?.isOpen,
+    defaultOpen: false,
+    onOpenChange: props.state?.setOpen,
+  });
+
+  const popoverRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  const popoverProps = usePopoverAria(
+    {
+      ...props,
+      triggerRef,
+      popoverRef,
+    },
+    state
+  );
+
+  const overlayTriggerProps = useOverlayTriggerAria(
+    { type: "dialog" },
+    state,
+    triggerRef
+  );
+
+  return {
+    state,
+    popoverRef,
+    triggerRef,
+    popoverProps,
+    overlayTriggerProps,
+  };
+}
