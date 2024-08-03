c33c56e4ad64f3a13d1ae3c12fb75222c22447ad
SeonghunYang <didtjdgns852@naver.com>
2024-08-03 08:20:39 +0000

    refactor: Update PopoverTrigger component to use polymorphic types and forwardRef

--- headless/components/popover/popover-trigger.tsx
+++ headless/components/popover/popover-trigger.tsx
@@ -1,8 +1,6 @@
 import React, { forwardRef, RefObject } from "react";
-import { Button } from "../button/button";
 import { usePopoverContext } from "./popover";
 import {
-  PolymorphicComponentPropsWithoutRef,
   PolymorphicComponentPropsWithRef,
   PolymorphicRef,
 } from "headless/types/polymorphic";
@@ -20,26 +18,19 @@ type PopoverTriggerComponent = <T extends React.ElementType = "button">(
 
 export const PopoverTrigger: PopoverTriggerComponent = forwardRef(
   function PopoverTrigger<T extends React.ElementType = "button">(
-    { as, children, ...props }: PopoverTriggerProps<T>,
+    { as, ...props }: PopoverTriggerProps<T>,
     ref: PolymorphicRef<T>
   ) {
-    const { triggerRef, triggerProps } = usePopoverContext();
+    const { triggerProps } = usePopoverContext();
 
     // const { overlayTriggerAriaProps, overlayTiggerProps }  = useOverlayTrigger
     // trigger를 poly로 만들면서 변경하기
     return (
       <button
         {...triggerProps}
+        {...props}
         ref={triggerProps.ref as RefObject<HTMLButtonElement>}
-      >
-        {children}
-      </button>
-      // <Button
-      //   {...triggerProps}
-      //   buttonRef={triggerRef as RefObject<HTMLButtonElement>}
-      // >
-      //   {children}
-      // </Button>
+      />
     );
   }
 );
