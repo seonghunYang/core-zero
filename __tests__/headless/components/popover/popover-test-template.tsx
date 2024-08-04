47933f56b520c47dfd23ab95b1c575c4385a26dc
SeonghunYang <didtjdgns852@naver.com>
2024-08-04 05:41:07 +0000

    refactor: Add unit tests for PopoverTestTemplate

---
+++ __tests__/headless/components/popover/popover-test-template.tsx
@@ -0,0 +1,97 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+
+export function PopoverTestTemplate() {
+  return (Component: JSX.Element) => {
+    describe(`${Component.type.name}`, () => {
+      it("should render", async () => {
+        render(Component);
+
+        expect(screen.getByRole("button")).toHaveTextContent("click");
+      });
+
+      it("open and close with trigger button", async () => {
+        const user = userEvent.setup();
+
+        render(Component);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+
+        await user.click(screen.getByText("click"));
+
+        expect(screen.getByText("Popover content")).toBeInTheDocument();
+
+        await user.click(screen.getByText("click"));
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+      });
+
+      it("open and close with outside click", async () => {
+        const user = userEvent.setup();
+
+        render(Component);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+
+        await user.click(screen.getByText("click"));
+
+        expect(screen.getByText("Popover content")).toBeInTheDocument();
+
+        // await user.click(document.body);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+      });
+
+      it("open and close with escape key", async () => {
+        const user = userEvent.setup();
+
+        render(Component);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+
+        await user.click(screen.getByText("click"));
+
+        expect(screen.getByText("Popover content")).toBeInTheDocument();
+
+        await user.keyboard("{Escape}");
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+      });
+
+      it("open and close with enter and escape key", async () => {
+        const user = userEvent.setup();
+
+        render(Component);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+
+        screen.getByText("click").focus();
+        await user.keyboard("{Enter}");
+
+        expect(screen.getByText("Popover content")).toBeInTheDocument();
+
+        await user.keyboard("{Escape}");
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+      });
+
+      it("open and close with space and escape key", async () => {
+        const user = userEvent.setup();
+
+        render(Component);
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+
+        screen.getByText("click").focus();
+        await user.keyboard(" ");
+
+        expect(screen.getByText("Popover content")).toBeInTheDocument();
+
+        await user.keyboard("{Escape}");
+
+        expect(screen.queryByText("Popover content")).not.toBeInTheDocument();
+      });
+    });
+  };
+}
