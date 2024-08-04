abe3fe429dcf5668639247af4f662fd1ae837abd
SeonghunYang <didtjdgns852@naver.com>
2024-08-04 04:11:26 +0000

    refactor: Update PopoverContent component to use destructured props and forwardRef

--- headless/components/popover/popover-content.tsx
+++ headless/components/popover/popover-content.tsx
@@ -1,39 +1,55 @@
 import { DismissButton, Overlay } from "react-aria";
 import { usePopoverContext } from "./popover";
-import React from "react";
+import React, { forwardRef } from "react";
 import { Dialog } from "../dialog/dialog";
+import {
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from "headless/types/polymorphic";
 
-interface PopoverContentProps {
+type _PopoverContentProps = {
   children: React.ReactNode;
-}
+};
 
-export function PopoverContent({ children }: PopoverContentProps) {
-  const { popoverContentProps } = usePopoverContext();
+type PopoverContentProps<C extends React.ElementType> =
+  PolymorphicComponentPropsWithRef<C, _PopoverContentProps>;
 
-  return (
-    <>
-      {popoverContentProps.isOpen && (
-        <Overlay>
-          <div {...popoverContentProps.underlayProps} className="underlay" />
-          <div
-            {...popoverContentProps.popoverProps}
-            ref={popoverContentProps.ref}
-            className="popover"
-          >
-            <svg
-              {...popoverContentProps.arrowProps}
-              className="arrow"
-              data-placement={popoverContentProps.placement}
-              viewBox="0 0 12 12"
+type PopoverContentComponent = <C extends React.ElementType = "div">(
+  props: PopoverContentProps<C>
+) => React.ReactNode;
+
+export const PopoverContent: PopoverContentComponent = forwardRef(
+  function PopoverContent<C extends React.ElementType = "div">(
+    { as, children, ...props }: PopoverContentProps<C>,
+    ref: PolymorphicRef<C>
+  ) {
+    const { popoverContentProps } = usePopoverContext();
+
+    return (
+      <>
+        {popoverContentProps.isOpen && (
+          <Overlay>
+            <div {...popoverContentProps.underlayProps} className="underlay" />
+            <div
+              {...popoverContentProps.popoverProps}
+              ref={popoverContentProps.ref}
+              className="popover"
             >
-              <path d="M0 0 L6 6 L12 0" />
-            </svg>
-            <DismissButton onDismiss={popoverContentProps.onClose} />
-            <Dialog {...popoverContentProps.overlayProps}>{children}</Dialog>
-            <DismissButton onDismiss={popoverContentProps.onClose} />
-          </div>
-        </Overlay>
-      )}
-    </>
-  );
-}
+              <svg
+                {...popoverContentProps.arrowProps}
+                className="arrow"
+                data-placement={popoverContentProps.placement}
+                viewBox="0 0 12 12"
+              >
+                <path d="M0 0 L6 6 L12 0" />
+              </svg>
+              <DismissButton onDismiss={popoverContentProps.onClose} />
+              <Dialog {...popoverContentProps.overlayProps}>{children}</Dialog>
+              <DismissButton onDismiss={popoverContentProps.onClose} />
+            </div>
+          </Overlay>
+        )}
+      </>
+    );
+  }
+);
